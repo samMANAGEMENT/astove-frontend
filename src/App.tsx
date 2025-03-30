a8558c288ebc160abb9301c89287b4f5032797fd
Samuel Pineda <sapinedal@outlook.com>
2025-03-30 10:37:45 -0500

    Configuracion Inicial

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
-import { Outlet } from "react-router-dom"
+import { Outlet, useLocation } from "react-router-dom"
 import { ThemeProvider } from "./components/theme-provider"
+import Sidebar from "./components/sidebar"
 
-function App() {
+export default function App() {
+  const location = useLocation()
+
+  // Rutas donde NO se debe mostrar el Sidebar
+  const hiddenSidebarRoutes = ["/login", "/register"]
+
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <div className="flex flex-col items-center justify-center min-h-svh">
-        <Outlet />
+      <div className="flex min-h-screen">
+        {/* Renderiza el Sidebar solo si la ruta actual NO está en `hiddenSidebarRoutes` */}
+        {!hiddenSidebarRoutes.includes(location.pathname) && <Sidebar />}
+        
+        {/* Contenido principal */}
+        <main className="flex-1 p-6">
+          <Outlet />
+        </main>
       </div>
     </ThemeProvider>
   )
 }
-
-export default App
--- src/pages/404/404.tsx
+++ src/pages/404/404.tsx
@@ -5,7 +5,7 @@ export default function NotFound() {
   const navigate = useNavigate();
 
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center bg-gray-100 text-center">
+    <div className="flex min-h-screen flex-col items-center justify-centertext-center">
       <h1 className="text-6xl font-bold text-red-500">404</h1>
       <p className="mt-4 text-lg text-gray-600">Oops! Page not found.</p>
       <Button className="mt-6" onClick={() => navigate("/")}>
